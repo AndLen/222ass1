4152b176391aa3c5b3ef8ff67baaf86fae230f2b
Bairdo <michaelb_111@hotmail.com>
2013-07-29 21:20:45 +1200

    comment

--- 222ass1/src/cluedo/cards/Weapon.java
+++ 222ass1/src/cluedo/cards/Weapon.java
@@ -4,6 +4,10 @@ public class Weapon implements WeaponI {
 
 	private String card;
 	
+	/**
+	 * 
+	 * @param i the number the card corresponds to from static WEAPONS[]
+	 */
 	public Weapon(int i){
 		this.card = Weapon.WEAPONS[i];
 	}
