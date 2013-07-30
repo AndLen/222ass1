8c8b29446b569bf40f842c2bedbd415f79e87c32
Bairdo <michaelb_111@hotmail.com>
2013-07-30 22:08:03 +1200

    made a location class to hold the x/y position of a token
    made a class for the passage move (the connecting corners)

---
+++ 222ass1/src/cluedo/moves/Passage.java
@@ -0,0 +1,21 @@
+package cluedo.moves;
+
+import cluedo.main.Board;
+
+public class Passage implements MoveI{
+
+	
+	
+	@Override
+	public boolean isValid(Board board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void apply(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
