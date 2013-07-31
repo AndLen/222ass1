2322b54fd6830479a0bbeeaf2a9417d10b9b31e5
Andrew <andrewlensen@gmail.com>
2013-07-31 19:25:55 +1200

    Text-based GUI basically functioning now. Might need some more prettifying though.

--- 222ass1/src/cluedo/main/Board.java
+++ 222ass1/src/cluedo/main/Board.java
@@ -2,54 +2,103 @@ package cluedo.main;
 
 /**
  * @author lensenandr
- *
+ * 
  */
 public class Board {
 	private static final int BOARD_WIDTH = 24;
 	private static final int BOARD_HEIGHT = 29;
 	private static Tile[][] board = new Tile[BOARD_WIDTH][BOARD_HEIGHT];
 
-	static{
-		//Spa
+	static {
+		// Spa
 		Tile spa = new Tile("Sp");
-		fillRoom(0,0,5,5, spa);
+		fillRoom(0, 0, 5, 5, spa);
 		fillRoom(0, 6, 4, 7, spa);
 
-		//Theatre
+		// Theatre
 		Tile theatre = new Tile("Th");
-		fillRoom(8, 0, 12, 7 , theatre);
+		fillRoom(8, 0, 12, 7, theatre);
 
-		//Living Room
+		// Living Room
 		Tile livingRoom = new Tile("Li");
-		fillRoom(14, 0, 19, 7 , livingRoom);
+		fillRoom(14, 0, 19, 7, livingRoom);
 		fillRoom(15, 8, 17, 8, livingRoom);
 
-		//Observatory
+		// Observatory
 		Tile observatory = new Tile("Ob");
-		fillRoom(22, 0, 23, 8 , observatory);
+		fillRoom(22, 0, 23, 8, observatory);
+
+		// Hall
+		Tile hall = new Tile("Ha");
+		fillRoom(19, 11, 23, 17, hall);
+
+		// Guest House
+		Tile guestHouse = new Tile("Gu");
+		fillRoom(20, 21, 23, 28, guestHouse);
+		fillRoom(21, 20, 23, 20, guestHouse);
+
+		// Dining room
+		Tile diningRoom = new Tile("Di");
+		fillRoom(10, 19, 15, 22, diningRoom);
+		fillRoom(9, 23, 16, 28, diningRoom);
+
+		// Kitchen
+		Tile kitchen = new Tile("Ki");
+		fillRoom(0, 21, 5, 21, kitchen);
+		fillRoom(0, 22, 6, 28, kitchen);
+
+		// Patio
+		Tile patio = new Tile("Pa");
+		fillRoom(0, 10, 3, 18, patio);
+		fillRoom(4, 11, 7, 17, patio);
+
+		// Pool
+		Tile pool = new Tile("Po");
+		fillRoom(10, 11, 17, 16, pool);
 	}
 
-	private static void fillRoom(int x, int y, int width, int height, Tile type){
-		for(;x<=width;x++){
-			//Uses a second variable or else y gets massive, heh. Dumb bug.
-			for(int y1 = y;y1<=height;y1++){
+	private static void fillRoom(int x, int y, int x2, int y2, Tile type) {
+		for (; x <= x2; x++) {
+			// Uses a second variable or else y gets massive, heh. Dumb bug.
+			for (int y1 = y; y1 <= y2; y1++) {
 				board[x][y1] = type;
 			}
 		}
 	}
+
+	/*
+	 * Used to print the board :-)
+	 */
 	public String toString(){
-		Tile[][]b2 = board;
+		//More efficient using a StrBlder.
 		StringBuilder sb = new StringBuilder();
+		//Extra to compensate for shift due to vertical axis.
+		sb.append("   ");
+		//Print x-coords across the top
+		for(int a = 0; a <BOARD_WIDTH; a++){
+			sb.append(digitToString(a));
+		}
+		sb.append("\n");
 		for(int i = 0; i < BOARD_HEIGHT; i++){
+			//Print y axis labels
+			sb.append(digitToString(i));
 			for(int j = 0; j < BOARD_WIDTH; j++){
 				if(board [j][i] == null){
-					sb.append("  ");
+					sb.append("   ");
 				}
-				else sb.append(board[j][i].toString());
+				else sb.append(board[j][i].toString() + " ");
 			}
 			//End of row
 			sb.append("\n");
 		}
 		return sb.toString();
+	}
+
+	private String digitToString(int i) {
+		//Needs additional space
+		if (i < 10) {
+			return i + "  ";
+		}
+		return i + " ";
 	}
 }
