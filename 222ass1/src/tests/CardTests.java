4bf73fef765e789b2c4c9e59929af8311f1d0609
Bairdo <michaelb_111@hotmail.com>
2013-08-12 19:06:42 +1200

    50 tests :)

--- 222ass1/src/tests/CardTests.java
+++ 222ass1/src/tests/CardTests.java
@@ -57,4 +57,79 @@ public class CardTests {
 
 	}
 
+	/**
+	 * tests weapon equals method
+	 */
+	@Test
+	public void testWeaponEquals() {
+
+		Card card = new Weapon(Weapon.WEAPONS[1]);
+		Card card2 = new Weapon(Weapon.WEAPONS[1]);
+
+		if (!card.equals(card2)) {
+			fail("Candlestick cards should be equal");
+		}
+
+	}
+
+	/**
+	 * tests weapon equals method
+	 */
+	@Test
+	public void testWeaponNotEquals() {
+
+		Card card = new Weapon(Weapon.WEAPONS[1]);
+		Card card2 = new Weapon(Weapon.WEAPONS[0]);
+
+		if (card.equals(card2)) {
+			fail("the weapons are not equal");
+		}
+
+	}
+
+	/**
+	 * tests weapon equals method
+	 */
+	@Test
+	public void testWeaponNotEquals2(){
+		
+		Card card = new Weapon(Weapon.WEAPONS[1]);
+		
+		if (card.equals(null)) {
+			fail("a weapon is not equal to null");
+		}
+		
+		
+	}
+	
+	/**
+	 * tests weapon equals method
+	 */
+	@Test
+	public void testWeaponNotEquals3(){
+		
+		Card card = new Weapon(Weapon.WEAPONS[1]);
+		
+		if (card.equals(3)) {
+			fail("a weapon is not equal to 3");
+		}
+		
+		
+	}
+	
+	/**
+	 * tests weapon equals method
+	 */
+	@Test
+	public void testWeaponEquals2(){
+		
+		Card card = new Weapon(Weapon.WEAPONS[1]);
+		
+		if (card.equals(card)) {
+			fail("a weapon is equal to iteself");
+		}
+		
+		
+	}
+	
 }
